cb2912cad63f961d809961830e4dbb859df32edd
Simon-2022 <96212730+Simon-2022@users.noreply.github.com>
2023-02-13 18:56:00 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -45,6 +45,27 @@ function draw() {
     image(video, 0, 0, 600, 500);
     fill("#ff7700");
     stroke("#f600ff");
+    circle(RightWristX, RightWristY, 20);
+    if(RightWristY>0 && RightWristY<=100) {
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5);
+    }
+    else if(RightWristY>100 && RightWristY<=200) {
+        document.getElementById("speed").innerHTML = "Speed = 1.0x";
+        song.rate(1.0);
+    }
+    else if(RightWristY>200 && RightWristY<=300) {
+        document.getElementById("speed").innerHTML = "Speed = 1.5x";
+        song.rate(1.5);
+    }
+    else if(RightWristY>300 && RightWristY<=400) {
+        document.getElementById("speed").innerHTML = "Speed = 2.0x";
+        song.rate(2.0);
+    }
+    else if(RightWristY>400 && RightWristY<=500) {
+        document.getElementById("speed").innerHTML = "Speed = 2.5x";
+        song.rate(2.5);
+    }
     if(scoreLeftWrist>0.2) {
     circle(LeftWristX, LeftWristY, 20);
     LeftWristY_number = Number(LeftWristY);
